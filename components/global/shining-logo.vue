419bedf5d69136bafba2f2db481c065b9248f5ca
zhangyi34 <zhangyi34@meituan.com>
2021-02-28 00:44:22 +0800

    feat: icon闪烁特效

---
+++ components/global/shining-logo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="shining-logo">
+    <div class="wrapper">
+      <i class="fab" :class="'fa-' + logo"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'shining-logo',
+  props: {
+    logo: String
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  @keyframes hua {
+    0% {
+      filter: hue-rotate(0deg)
+    }
+    100% {
+      filter: hue-rotate(360deg)
+    }
+  }
+  .wrapper {
+    padding: 150px 0;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .fab {
+      @shadow-color: #0f0;
+      font-size: 120px;
+      color: #fff;
+      text-shadow: 0 0 50px @shadow-color,
+                    0 0 250px @shadow-color,
+                    0 0 5000px @shadow-color;
+      animation: hua 5s linear infinite;
+    }
+  }
+</style>
---
+++ content/cn/effects/shining-logo.md
@@ -0,0 +1,19 @@
+---
+title: LOGO闪烁效果
+category: 精彩特效
+position: 2
+---
+
+## 说明
+利用字体icon + text-shadow形成主要效果，加上animation和hua-rotate完成变色动画
+
+## 效果
+### 苹果
+<shining-logo logo="apple"></shining-logo>
+### steam
+<shining-logo logo="steam"></shining-logo>
+
+## 代码
+```vue{1,3-5}
+file(components/global/shining-logo.vue)
+```
