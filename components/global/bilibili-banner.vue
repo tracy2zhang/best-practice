311cf51383006d7885812498007b8e14961bbf29
happyelements <yii.zhang@happyelements.com>
2021-03-02 17:56:45 +0800

    feat: bilibili banner

---
+++ components/global/bilibili-banner.vue
@@ -0,0 +1,86 @@
+<template>
+  <div ref="banner" class="bilibili-banner" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" @mousemove="onMouseMove">
+    <div class="layer bg">
+      <img class="morning" src="../../assets/img/bilibili/bilibili_bg_1.jpg" alt="">
+      <img class="afternoon" src="../../assets/img/bilibili/bilibili_bg_2.jpg" alt="">
+      <video autoplay loop muted class="evening">
+        <source src="../../assets/video/bilibili_bg_v.webm" type="video/webm" />
+      </video>
+    </div>
+    <div class="layer tree">
+      <img class="morning" src="../../assets/img/bilibili/bilibili_tree_1.png" alt="">
+      <img class="afternoon" src="../../assets/img/bilibili/bilibili_tree_2.png" alt="">
+      <img class="evening" src="../../assets/img/bilibili/bilibili_tree_3.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bilibili-banner',
+  mounted () {
+    this.startingPoint = 0
+    this.clientWidth = this.$refs.banner.clientWidth
+  },
+  methods: {
+    onMouseEnter (e) {
+      this.startingPoint = e.offsetX
+    },
+    onMouseLeave (e) {
+      this.$refs.banner.style.setProperty('--percentage', 0.5)
+    },
+    onMouseMove (e) {
+      const percentage = (e.offsetX - this.startingPoint) / this.clientWidth + 0.5
+      this.$refs.banner.style.setProperty('--percentage', percentage)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .bilibili-banner {
+    --percentage: 0.5;
+    margin: 20px 0;
+    position: relative;
+    padding: 0;
+    height: 80px;
+    overflow: hidden;
+    transform: translate3d(0);
+    &:hover {
+      .layer, .afternoon, .evening {
+        transition: none;
+      }
+    }
+    .layer {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transform: ~"translatex(calc(var(--percentage) * -50px))";
+      transition: .2s all ease-in;
+      &.tree {
+        transform: ~"translatex(calc(var(--percentage) * -20px))";
+        filter: blur(2px);
+      }
+      img, video {
+        position: absolute;
+        width: 120%;
+        max-width: 120%;
+        height: 100%;
+        margin: 0;
+        object-fit: cover;
+        transition: .2s all ease-in;
+      }
+      .afternoon {
+        opacity: ~"calc((var(--percentage) - 0.25) / 0.25)";
+      }
+      .evening {
+        opacity: ~"calc((var(--percentage) - 0.5) / 0.25)";
+      }
+    }
+  }
+</style>
---
+++ content/cn/effects/bilibili-banner.md
@@ -0,0 +1,13 @@
+---
+title: B站banner
+category: 精彩特效
+position: 4
+---
+
+## 效果
+<bilibili-banner></bilibili-banner>
+
+## 代码
+```vue{1,3-5}
+file(components/global/bilibili-banner.vue)
+```
